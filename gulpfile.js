3df8c67792c71a74795221beb6a50c812a2a9d05
nk44ukrnet <56731417+nk44ukrnet@users.noreply.github.com>
2022-07-08 16:25:51 +0300

    adjustments upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,46 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
-var uglify = require('gulp-uglify');
+var cssMin = require('gulp-css');
+var rename = require("gulp-rename");
 var concat = require('gulp-concat');
-var cssmin = require('gulp-css');
-var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
-var localFiles = ['./assets/dist/**/*'];
-gulp.task('sass', function() {
-	gulp.src([
-		'assets/src/scss/app.scss'
-	])
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('assets/src/css'))
+
+gulp.task('css', function(){
+    return gulp.src('assets/src/scss/app.css')
+        .pipe(concat('app.css'))
+        .pipe(rename({suffix:'.min'}))
+        .pipe(cssMin())
+        .pipe(gulp.dest('assets/src/scss/'))
+        .pipe(concat('app.css'))
+        .pipe(rename({suffix:'.min'}))
+        .pipe(cssMin())
+        .pipe(gulp.dest('assets/dist/css/'));
 });
 
-gulp.task('css', ['sass'], function() {
-	setTimeout(function(){
-		gulp.src([
-			'assets/src/css/app.css'
-		])
-			.pipe(concat('app.css'))
-			.pipe(rename({suffix:'.min'}))
-			.pipe(cssmin())
-			.pipe(gulp.dest('assets/dist/css'))
-	}, 200)
+gulp.task('js', function (done) {
+    setTimeout(async function() {
+        gulp.src([
+            'assets/src/js/vendor/swiper-bundle.min.js',
+            'assets/src/js/app.js'
+        ])
+            .pipe(concat('app.js'))
+            .pipe(rename({suffix: '.min'}))
+            .pipe(uglify())
+            .pipe(gulp.dest('assets/dist/js/'))
+    }, 200)
+    setTimeout(async function(){
+        gulp.src([
+            'assets/src/js/vendor/swiper-bundle.min.js',
+            'assets/src/js/app.js'
+        ])
+            .pipe(concat('app.js'))
+            .pipe(rename({suffix:'.min'}))
+            .pipe(uglify())
+            .pipe(gulp.dest('assets/src/js/'))
+    }, 250)
+    done();
 });
 
-gulp.task('js', function() {
-	setTimeout(function(){
-		gulp.src([
-			'assets/src/js/vendor/lazyload.js',
-			'assets/src/js/app.js'
-		])
-			.pipe(concat('app.js'))
-			.pipe(rename({suffix:'.min'}))
-			.pipe(uglify())
-			.pipe(gulp.dest('assets/dist/js/'))
-	}, 200)
-	setTimeout(function(){
-		gulp.src([
-			'assets/src/js/courses.js'
-		])
-			.pipe(concat('courses.js'))
-			.pipe(rename({suffix:'.min'}))
-			.pipe(uglify())
-			.pipe(gulp.dest('assets/dist/js/'))
-	}, 250)
-});
+gulp.task('dev', gulp.series('css', 'js'));
 
-gulp.task('default', ['sass', 'css', 'js']);
 
-gulp.task('watch', function() {
-	gulp.watch('assets/src/scss/**/*.scss', ['css'])
-	gulp.watch('assets/src/js/*.js', ['js'])
-});
